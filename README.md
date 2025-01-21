851d646434f91f11b531183e41c8ec294b7f2cb5
Jason <jasonch91@gmail.com>
2025-01-20 22:23:45 -0800

    Updated 1/20/2025

---
+++ README.md
@@ -0,0 +1,1 @@
+# dublbubl
---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+import sqlite3
+
+from flask import Flask, request, redirect, render_template, request, session
+from flask_session import Session
+from functools import wraps
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Connect sqlite3 database
+con = sqlite3.connect("dublbubl.db")
+cur = con.cursor()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route("/")
+def index():
+        return render_template('index.html')
